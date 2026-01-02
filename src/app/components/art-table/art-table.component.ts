33744c9f77f299b74b4db53f8b392cec7db931f7
Dav-Rodriguez <156963675+Dav-Rodriguez@users.noreply.github.com>
2026-01-02 13:50:05 -0500

    art-Tables

--- src/app/components/art-table/art-table.component.ts
+++ src/app/components/art-table/art-table.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+// Importamos la tabla y sus componentes de fila específicos
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { Artwork } from '../../services/art.service';
@@ -9,40 +10,48 @@ import { Artwork } from '../../services/art.service';
   standalone: true,
   imports: [CommonModule, RouterModule, MatTableModule],
   template: `
-    <table mat-table [dataSource]="artworks" class="mat-elevation-z8">
-      <ng-container matColumnDef="id">
-        <th mat-header-cell *mat-header-cell>ID</th>
-        <td mat-cell *mat-cell="let element">{{ element.id }}</td>
-      </ng-container>
+    <div class="table-container">
+      <table mat-table [dataSource]="artworks" class="mat-elevation-z8">
+        <ng-container matColumnDef="id">
+          <th mat-header-cell *mat-header-cell>ID</th>
+          <td mat-cell *mat-cell="let element">{{ element.id }}</td>
+        </ng-container>
 
-      <ng-container matColumnDef="title">
-        <th mat-header-cell *mat-header-cell>Title</th>
-        <td mat-cell *mat-cell="let element">{{ element.title }}</td>
-      </ng-container>
+        <ng-container matColumnDef="title">
+          <th mat-header-cell *mat-header-cell>Title</th>
+          <td mat-cell *mat-cell="let element">{{ element.title }}</td>
+        </ng-container>
 
-      <ng-container matColumnDef="artist">
-        <th mat-header-cell *mat-header-cell>Artist</th>
-        <td mat-cell *mat-cell="let element">{{ element.artist_display }}</td>
-      </ng-container>
+        <ng-container matColumnDef="artist">
+          <th mat-header-cell *mat-header-cell>Artist</th>
+          <td mat-cell *mat-cell="let element">{{ element.artist_display }}</td>
+        </ng-container>
 
-      <tr mat-header-row *mat-header-rowDef="displayedColumns"></tr>
-      <tr
-        mat-row
-        *mat-rowDef="let row; columns: displayedColumns"
-        [routerLink]="['/detail', row.id]"
-        class="element-row"
-      ></tr>
-    </table>
+        <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+
+        <tr
+          mat-row
+          *matRowDef="let row; columns: displayedColumns"
+          [routerLink]="['/detail', row.id]"
+          class="element-row"
+        ></tr>
+      </table>
+    </div>
   `,
   styles: [
     `
+      .table-container {
+        width: 100%;
+        overflow-x: auto;
+      }
       table {
         width: 100%;
       }
       .element-row {
         cursor: pointer;
+        transition: background 0.2s;
         &:hover {
-          background: #f5f5f5;
+          background-color: rgba(0, 0, 0, 0.05);
         }
       }
     `,
